e2536f32cd5083e082fc146ad24b7e230d87cfa5
Paolo Molinara <pmolinara@gmail.com>
2016-06-21 17:07:46 +0200

    elasticJson ora scrive un son pronto per il bulk elastic

--- script/elasticJSON.js
+++ script/elasticJSON.js
@@ -1,91 +1,58 @@
-// http://www.pizzanapoletana.org/albo_pizzaioli_show.php?naz=Elenco
-
+var Promise = require('bluebird');
 var jsonfile = require('jsonfile');
+var chalk = require('chalk');
 var fs = require('fs');
+var path = require('path');
+Promise.promisifyAll(fs);
 
 var d = new Date();
 d = d.toLocaleDateString();
-var path = './../storage/' + d + '/url-html/';
+var pathPrevDir = path.join('./../storage/', d, '/url-html/');
 var elasticPath = './../storage/' + d + '/elastic/elastic_' + d + '.json';
+var doc = "";
+var index = -1;
 
-// torna un array di file in /storage/[data]/html/[nome]/
-var getListFileJson = function(listHtml) {
-    return new Promise(function(resolve, reject) {
-        // leggo tutte le cartelle in /storage/[data]/html/
-        fs.readdir(path, function(err, items1) {
-            for (var i = 0; i < items1.length; i++) {
-                //per ogni cartella in /storage/[data]/html/
-                var filePath = path + items1[i];
-                resolve(filePath);
-            }
-        });
-    });
-};
+function createElasticJson() {
+    return fs.writeFileAsync(elasticPath, doc);
+}
+exports.createElasticJson = createElasticJson;
 
-var readFileHtml = function(filePath) {
-    return new Promise(function(resolve, reject) {
-        // leggo il file i in /storage/[data]/html/[nome]/
-        jsonfile.readFile(filePath, function(err, obj) {
-            resolve(obj);
-        });
-    });
-};
+function readDirs() {
+    return fs.readdirAsync(pathPrevDir);
+}
+exports.readDirs = readDirs;
 
-var p = {
-    "name": "Abbate Vincenzo",
-    "allPages": [{
-        "content": "sito",
-    }]
-};
-
-var createElasticJson = function(filePath) {
-    return new Promise(function(resolve, reject) {
-        // console.log("filePath", filePath);
-        for (var i = 0; i < 1; i++) {
-            readFileHtml(filePath)
-                .then(function(results) {
-                    var doc = '';
-                    console.log('results.allPages.length ------>' + results.allPages.length);
-                    for (var j = results.allPages.length - 1; j >= 0; j--) {
-                        var index = '{ "index": { "_id": "' + j + '" } }\n';
-                        var site = JSON.stringify(results.allPages[j]);
-                        //console.log(site.substring(0, 300));
-                        //console.log('j ------>' + j);
-                        var allLine = index + site + '\n';
-                        doc = doc + allLine;
-                        if (j === 0) {
-                            console.log('POSSIAMO SCRIVERE ORA!');
-                            fs.open(elasticPath, 'a', 666, function(e, id) {
-                                fs.write(id, doc, 'utf8', function() {
-                                    fs.close(id, function() {
-                                        console.log('file is updated');
-                                    });
-                                });
-                            });
-
-                        }
-                    }
-                });
-        }
-        resolve(allHtml);
-    });
-};
+function readFiles(pathDir) {
+    return fs.readFileAsync(path.join(pathPrevDir, pathDir), 'utf8');
+}
+exports.readFiles = readFiles;
 
 
-// scrive il JSON su disco
-var writeFileJsonHtml = function() {
-    fs.writeFile(elasticPath, '', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    });
+function flatPromiseArray(nestedArray) {
+    return nestedArray.reduce(function(previousVal, currentVal) {
+        return previousVal.concat(currentVal);
+    }, []); // <= questo diventa previousVal alla prima iterazione
+}
+exports.flatPromiseArray = flatPromiseArray;
 
-};
+function elasticFormat(all) {
+    var elasticData = JSON.parse(all);
+    for (var i = elasticData.length - 1; i >= 0; i--) {
+        index = index + 1;
+        doc = doc + '{ "index": { "_id": "' + index + '" } }\n' + JSON.stringify(elasticData[i]) + '\n';
+    }
+}
+exports.elasticFormat = elasticFormat;
 
-Promise
-    .all([writeFileJsonHtml(), getListFileJson()])
-    .then(function(value) {
-        console.log('value[1]', value[1]);
-        return createElasticJson(value[1]);
-    });
+function writeJSONFile() {
+    console.log("scrivooooooooooooooooooooooooooooooooooo");
+    return fs.appendFile(elasticPath, doc);
+}
+exports.writeJSONFile = writeJSONFile;
+
+createElasticJson()
+    .then(readDirs)
+    .map(pathFile => readFiles(pathFile))
+    .then(flatPromiseArray)
+    .map(obj => elasticFormat(obj))
+    .then(writeJSONFile);
